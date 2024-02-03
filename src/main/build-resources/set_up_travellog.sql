64ec7933141c04e3199bb27ff72b80afe5934a97
Paul Stead <paul.stead@tyke.co.uk>
2024-02-03 16:41:04 +0000

    TRAV-11 Updated DB script

--- src/main/build-resources/set_up_travellog.sql
+++ src/main/build-resources/set_up_travellog.sql
@@ -11,15 +11,15 @@ CREATE TABLE "journey" ( -- a journey consists of one or more trips
 	"created" timestamp DEFAULT (now())
 );
 
-CREATE TABLE "trip" (
-	"trip_id" BIGSERIAL primary key,
+CREATE TABLE "leg" (
+	"leg_id" BIGSERIAL primary key,
 	"journey_id" bigint,
 	"created" timestamp DEFAULT (now())
-)
+);
 
-CREATE TABLE "trip_point" (
-  "trip_point_id" BIGSERIAL primary key,
-  "trip_id" bigint,
+CREATE TABLE "location" (
+  "location_id" BIGSERIAL primary key,
+  "leg_id" bigint,
   "latitude" numeric(7,4),
   "longitude" numeric(7,4),
   "altitude" int,
@@ -27,11 +27,11 @@ CREATE TABLE "trip_point" (
 );
 
 
-ALTER TABLE "trip_point" ADD FOREIGN KEY ("trip_id") REFERENCES "trip" ("trip_id");
-ALTER TABLE "trip" ADD FOREIGN KEY ("journey_id") REFERENCES "journey" ("journey_id");
+ALTER TABLE "location" ADD FOREIGN KEY ("leg_id") REFERENCES "leg" ("leg_id");
+ALTER TABLE "leg" ADD FOREIGN KEY ("journey_id") REFERENCES "journey" ("journey_id");
 
-alter table trip_point OWNER TO tyke_developer;
-alter table trip OWNER TO tyke_developer;
+alter table location OWNER TO tyke_developer;
+alter table leg OWNER TO tyke_developer;
 alter table journey OWNER TO tyke_developer;
 
 GRANT USAGE ON SCHEMA travellog_journey_dev_schema TO tyke_developer;
