65de49c5d89ab6e6bd5a13113dd6dae194984849
Paul Stead <paul.stead@tyke.co.uk>
2024-06-27 21:19:36 +0100

    TRAV-5 Retrieve Locations

--- src/main/java/uk/co/tyke/travellog/journey/data/postgres/JourneyPostgresImpl.java
+++ src/main/java/uk/co/tyke/travellog/journey/data/postgres/JourneyPostgresImpl.java
@@ -1,7 +1,9 @@
 package uk.co.tyke.travellog.journey.data.postgres;
 
-import org.jooq.Configuration;
-import org.jooq.DSLContext;
+import org.jooq.*;
+import org.jooq.Record;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.co.tyke.travellog.journey.data.JourneyData;
 import uk.co.tyke.travellog.journey.data.postgres.jooq.tables.records.JourneyRecord;
 import uk.co.tyke.travellog.journey.data.postgres.jooq.tables.records.LegRecord;
@@ -13,13 +15,20 @@ import uk.co.tyke.travellog.journey.model.Location;
 import javax.inject.Singleton;
 
 import java.util.List;
+import java.util.Objects;
 
+// import static org.jooq.Records.mapping;
+import static org.jooq.Records.mapping;
+import static org.jooq.impl.DSL.multiset;
+import static org.jooq.impl.DSL.selectFrom;
 import static uk.co.tyke.travellog.journey.data.postgres.jooq.tables.Journey.JOURNEY;
 import static uk.co.tyke.travellog.journey.data.postgres.jooq.tables.Leg.LEG;
 import static uk.co.tyke.travellog.journey.data.postgres.jooq.tables.Location.LOCATION;
 
 @Singleton
 public class JourneyPostgresImpl implements JourneyData {
+
+    private Logger logger = LoggerFactory.getLogger(JourneyPostgresImpl.class);
 
     private final DSLContext context;
 
@@ -38,13 +47,13 @@ public class JourneyPostgresImpl implements JourneyData {
         long journeyId = journeyRecord.getJourneyId();
 
         List<Leg> legs = journey.legs();
-        for (Leg leg: legs) {
+        for (Leg leg : legs) {
             LegRecord legRecord = context.newRecord(LEG);
             legRecord.setJourneyId(journeyId);
             legRecord.setDistance(leg.distance());
             legRecord.store();
             long legId = legRecord.getLegId();
-            for (Location location: leg.locations()) {
+            for (Location location : leg.locations()) {
                 LocationRecord locationRecord = context.newRecord(LOCATION);
                 locationRecord.setLegId(legId);
                 locationRecord.setLatitude(location.latitude());
@@ -56,6 +65,44 @@ public class JourneyPostgresImpl implements JourneyData {
         }
 
         return journeyId;
+
+    }
+
+    @Override
+    public List<Journey> getJourneys() {
+        logger.debug("Get Journeys");
+        assert context != null;
+
+        List<Location> result = context.select(
+                        LOCATION.LATITUDE,
+                        LOCATION.LONGITUDE,
+                        LOCATION.ALTITUDE,
+                        LOCATION.CREATED)
+                .from(LOCATION)
+                .orderBy(LOCATION.LOCATION_ID)
+                .fetch(mapping(Location::new));
+
+
+        logger.info("Number of locations returned: {}", result.size());
+
+        // String name, String notes, List<Leg> legs
+//        List<Journey> result = context.select(
+//                JOURNEY.NAME,
+//                JOURNEY.NOTES,
+//                multiset(selectFrom(LEG).where(LEG.JOURNEY_ID.eq(JOURNEY.JOURNEY_ID))).as("legs"))
+//                .from(JOURNEY)
+//                .orderBy(JOURNEY.JOURNEY_ID)
+//                .fetchInto(Journey.class);
+
+//        Result<Record> journeys = context.select()
+//                .from(JOURNEY).fetch();
+//
+//        journeys.forEach(journey -> {
+//            journey.getValue(JOURNEY.JOURNEY_ID)
+//        });
+
+
+        return null;
 
     }
 }
